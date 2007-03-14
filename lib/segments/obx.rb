ab3b68d9c4276b44c7a44f68033862647cea3142
segfault <segfault@72d81af0-b0fa-4b65-b54c-2b622d79b57a>
2007-03-14 05:06:36 +0000

    added the field aliases
    git-svn-id: svn://hasno.info/ruby-hl7/trunk@16 72d81af0-b0fa-4b65-b54c-2b622d79b57a

--- lib/segments/obx.rb
+++ lib/segments/obx.rb
@@ -1,6 +1,24 @@
 # $Id$
 require 'ruby-hl7'
 class HL7::Message::Segment::OBX < HL7::Message::Segment
-  add_field :name=>:set_id, :idx=> 1
+  weight 90
+  add_field :name=>:set_id
+  add_field :name=>:value_type
+  add_field :name=>:observation_id
+  add_field :name=>:observation_sub_id
+  add_field :name=>:observation_value
+  add_field :name=>:units
+  add_field :name=>:references_range
+  add_field :name=>:probability
+  add_field :name=>:nature_of_abnormal_test
+  add_field :name=>:observation_result_status
+  add_field :name=>:effective_date_of_reference_range
+  add_field :name=>:user_defined_access_checks
+  add_field :name=>:observation_date
+  add_field :name=>:producer_id
+  add_field :name=>:responsible_observer
+  add_field :name=>:observation_method
+  add_field :name=>:equipment_instance_id
+  add_field :name=>:analysis_date
 end
 
