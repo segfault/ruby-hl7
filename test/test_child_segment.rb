d30d0d6eecf66055b5f4aa3fa918bdab32de41ce
Mark Guzman <segfault@hasno.info>
2007-06-29 02:38:16 +0000

    initial segment add tests
    git-svn-id: svn://hasno.info/ruby-hl7/trunk@39 72d81af0-b0fa-4b65-b54c-2b622d79b57a

--- test/test_child_segment.rb
+++ test/test_child_segment.rb
@@ -19,7 +19,22 @@ class ChildSegment < Test::Unit::TestCase
     msg[:OBR].first.children.each do |x|
       assert_not_nil x
     end
+  end
 
+  def test_add_children
+    msg = HL7::Message.new @base
+    assert_not_nil msg
+    assert_not_nil msg[:OBR]
+    ob = HL7::Message::Segment::OBR.new
+    assert_not_nil ob
+    msg << ob
+    assert_not_nil ob.children
+    (1..4).each do |x|
+      m = HL7::Message::Segment::OBX.new
+      assert_not_nil m
+      ob.children << m
+    end
+    assert_not_equal( @base, msg.to_hl7 )
   end
 end
 
