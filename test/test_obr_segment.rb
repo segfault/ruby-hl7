b4a244b68b0c97e0c3ce56979c1257254dbd6d0f
Andy Gregorowicz <andy@mitre.org>
2008-10-08 01:48:00 +0800

    adding OBR-4 (Universal Service Id) cleaning up test data for the OBR segment
    Signed-off-by: Mark Guzman <segfault@hasno.info>

--- test/test_obr_segment.rb
+++ test/test_obr_segment.rb
@@ -5,7 +5,7 @@ require 'ruby-hl7'
 
 class ObrSegment < Test::Unit::TestCase
   def setup
-    @base = "OBR|2|^DMCRES|0000000567^RADIS1|37956^CT ABDOMEN^L|||199405021550|||||||||||||0000763|||||P||||||R/O TUMOR|202300^BAKER^MARK^E|||01^LOCHLEAR, JUDY"
+    @base = "OBR|2|^USSSA|0000000567^USSSA|37956^CT ABDOMEN^LN|||199405021550|||||||||||||0000763|||||P||||||R/O TUMOR|202300&BAKER&MARK&E|||01&LOCHLEAR&JUDY"
   end
 
   def test_read
@@ -13,9 +13,9 @@ class ObrSegment < Test::Unit::TestCase
     assert_equal( @base, obr.to_s )
     assert_equal( "2", obr.e1 )
     assert_equal( "2", obr.set_id )
-    assert_equal( "^DMCRES", obr.placer_order_number )
-    assert_equal( "0000000567^RADIS1", obr.filler_order_number )
-    assert_equal( "37956^CT ABDOMEN^L", obr.priority )
+    assert_equal( "^USSSA", obr.placer_order_number )
+    assert_equal( "0000000567^USSSA", obr.filler_order_number )
+    assert_equal( "37956^CT ABDOMEN^LN", obr.universal_service_id )
   end
 
   def test_create
