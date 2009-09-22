93d70ce2e4fc61abce8b04f9eb615a5021970f11
Mark Guzman <segfault@hasno.info>
2009-09-21 22:14:06 -0400

    version bump to 0.4, ruby 1.9 support

--- lib/ruby-hl7.rb
+++ lib/ruby-hl7.rb
@@ -21,7 +21,7 @@ require "stringio"
 require "date"
 
 module HL7 # :nodoc:
-  VERSION = "0.3" 
+  VERSION = "0.4" 
   def self.ParserConfig
     @parser_cfg ||= { :empty_segment_is_error => true }
   end
